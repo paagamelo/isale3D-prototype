3df208bbeee30447cdec586cd72f8f90cedbf20e
paagamelo <lorenzo.paganelli2@gmail.com>
2021-06-24 10:27:29 +0200

    drop deprecated functions

--- include/utils.h
+++ include/utils.h
@@ -7,12 +7,6 @@
 #include <string>
 
 // Prints a help message explaining how to use the software.
-/// TODO: document.
 void print_help();
 
-// Prints the partners setup for each process.
-/// TODO: document.
-void print_partners_setup(int rank, int n_partners, const int* partners,
-                          const int* partners_map, bool use_shm);
-
-#endif // HALO_EXCHANGE_BENCHMARK_UTILS_H
+#endif // ISALE3D_PROTOTYPE_UTILS_H
--- src/utils.cpp
+++ src/utils.cpp
@@ -2,30 +2,10 @@
  * Created by Lorenzo Paganelli (acse-lp320, paagamelo on GitHub).
  */
 #include "utils.h"
-#include <mpi.h>
 #include <iostream>
 
-/// TODO: out of date.
 void print_help()
 {
-    std::cerr << "Usage: mpirun -n X ./benchmark [use_shm n_partners n_reps "
-                 "n_iterations message_size verbose]\n"
-                 "For further info see the documentation on top of "
+    std::cerr << "Inconsistent arguments. See the documentation on top of "
                  "benchmark.cpp\n";
-}
-
-void print_partners_setup(int rank, int n_partners, const int* partners,
-                          const int* partners_map, bool use_shm)
-{
-    std::cout << "rank " << rank << " has partners: [ ";
-    for (int i = 0; i < n_partners; i++)
-    {
-        std::cout << partners[i];
-        if (use_shm && partners_map[i] != MPI_UNDEFINED) std::cout << "*";
-        std::cout << " ";
-    }
-    std::cout << "]\n";
-    std::cout.flush();
-
-    if (rank == 0 && use_shm) std::cout << "* = residing on the same node.\n";
 }
