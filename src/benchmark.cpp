3e8f8dd0570a470bb9951750be1785513bb961b1
paagamelo <lorenzo.paganelli2@gmail.com>
2021-06-24 10:27:05 +0200

    review command line args

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -5,37 +5,17 @@
  * TODO: document.
  *
  * To compile: mkdir build && cd build && cmake .. && make benchmark
+ * Or: make (for systems without cmake >= 3.10 installed)
  *
- * Usage: mpirun -n P ./benchmark [use_allreduce n_reps n_iterations verbose]
+ * Usage: mpirun -n P ./benchmark [kernel_id n_reps n_iterations output_fname]
  * P = number of processes to run.
- * use_allreduce = whether to exchange the timestep with an allreduce or not
- * (see exchange_dt.h).
- * n_reps = number of times the benchmark is run to find the minimum
- * average elapsed time.
- * n_iterations = number of times halos are exchanged to find the average
- * elapsed time.
- * verbose = whether to print extra info during execution (either 0 or 1).
- *
- * *OUT OF DATE*
- * Usage: mpirun -n P ./benchmark [use_shm lock_each_iteration n_partners n_reps n_iterations message_size verbose]
- * P = number of processes to run.
- * use_shm = whether to use shared memory communication between processes on
- * the same node (either 0 or 1, 0 being false).
- * lock_each_iteration = whether to lock/unlock the shared window on a
- * per iteration basis (either 0 or 1, 0 meaning the window will be locked and
- * timed on a per repetition basis). Meaningful only if use_shm = 1.
- * n_partners = number of partners for each process (either 2, 4 or 8).
- * n_reps = number of times the benchmark is run to find the minimum
- * average elapsed time.
- * n_iterations = number of times halos are exchanged to find the average
- * elapsed time.
- * message_size = size of messages in bytes.
- * verbose = whether to print extra info during execution (either 0 or 1).
- *
- * Alternatively, the software can be run with no command line argument. In this
- * case, a "config.txt" file is assumed to be located in the directory of
- * execution. Such file should contain the arguments described above in its
- * first line, as if they were specified via command line.
+ * kernel_id = which kernel to time. 0 = GatherBcast, 1 = Allreduce,
+ * 2 = AllreduceSend (see exchange_dt.h).
+ * n_reps = number of times the benchmark is repeated to find the minimum
+ * average elapsed time (see timer.h).
+ * n_iterations = number of times the kernel is run to find the average
+ * elapsed time (see timer.h).
+ * output_fname = name of the output file.
  *
  * Bibliography:
  * [1] https://cvw.cac.cornell.edu/mpionesided/onesidedef
@@ -45,9 +25,7 @@
  * [5] https://dl.acm.org/doi/10.5555/648136.748782
  * [6] https://www.mcs.anl.gov/research/projects/mpi/mpptest/
  */
-/// TODO: include guards in every file are to be updated.
 #include "timer.cpp" // due to templates
-/// #include "kernels.h"
 #include "utils.h"
 #include "exchange_dt.h"
 #include <mpi.h>
@@ -76,92 +54,30 @@ int main(int argc, char *argv[])
         MPI_Abort(MPI_COMM_WORLD, 1);
         return 1;
     }
-    int args[N_ARGS];
-    if (argc == 1) // Arguments to be read from a configuration file.
-    {
-        // Having all the processes trying to read the same file may not be a
-        // good idea. Process 0 will read it and broadcast its content.
-        if (rank == 0)
-        {
-            std::string fname = "config.txt";
-            std::ifstream file;
-            file.open(fname);
-            if (file.fail())
-            {
-                std::cerr << "Can't open " << fname << "\n";
-                MPI_Abort(MPI_COMM_WORLD, 1);
-                return 1;
-            }
-            std::string line;
-            getline(file, line);
-            file.close();
-            std::istringstream iss(line);
-            for (int& arg : args) iss >> arg;
-        }
-        MPI_Bcast(args, N_ARGS, MPI_INT, 0, MPI_COMM_WORLD);
-    }
-    else // Arguments specified from the command line.
-    {
-        for (int i = 0; i < N_ARGS; i++)
-            // Convert arguments to int. argv[1] is the name of the executable,
-            // so arguments are shifted by one.
-            args[i] = std::stoi(argv[i + 1]);
-    }
-    /// bool use_shm, lock_each_iteration, verbose;
-    /// int n_partners, n_reps, n_iterations, message_size;
-    bool use_allreduce, verbose;
-    int n_reps, n_iterations;
-    /*
-    use_shm = args[0] != 0;
-    lock_each_iteration = args[1] != 0;
-    n_partners = args[2];
-    n_reps = args[3];
-    n_iterations = args[4];
-    message_size = args[5];
-    verbose = args[6] != 0;
-    */
-    use_allreduce = args[0] != 0;
-    n_reps = args[1];
-    n_iterations = args[2];
-    verbose = args[3] != 0;
 
-    /*
-    if (verbose && rank == 0)
-    {
-        std::cout << "will run " << n_reps << " repetitions with " << n_iterations
-                  << " iterations each\n";
-    }
-    */
+    int kernel_id = std::stoi(argv[1]);
+    int n_reps = std::stoi(argv[2]);
+    int n_iterations = std::stoi(argv[3]);
+    std::string output_fname = argv[4];
 
     Computation* comp;
-    if (use_allreduce) comp = new AllReduce(rank, n_processes);
-    else comp = new GatherBcast(rank, n_processes);
-    /*
-    if (use_shm)
-        comp = new Shm(rank, n_processes, n_partners, message_size,
-                       lock_each_iteration);
-    else
-        comp = new PointToPoint(rank, n_processes, n_partners, message_size);
-    */
+    switch (kernel_id)
+    {
+        case 0: comp = new GatherBcast(rank, n_processes); break;
+        case 1: comp = new AllReduce(rank, n_processes); break;
+        case 2: comp = new AllReduceSend(rank, n_processes); break;
+        default:
+        {
+            if (rank == 0) print_help();
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+    }
 
     double dt = time(rank, n_reps, n_iterations, *comp);
     if (rank == 0)
     {
-        /*
-        std::string fname = "out_" +
-                            std::to_string(use_shm) + "_" +
-                            std::to_string(lock_each_iteration) + "_" +
-                            std::to_string(n_partners) + "_" +
-                            std::to_string(n_reps) + "_" +
-                            std::to_string(n_iterations) + "_" +
-                            std::to_string(message_size) + ".txt";
-        */
-        std::string fname = "out_" +
-                            std::to_string(use_allreduce) + "_" +
-                            std::to_string(n_reps) + "_" +
-                            std::to_string(n_iterations) + ".txt";
         std::ofstream file;
-        file.open(fname, std::ios_base::app);
+        file.open(output_fname + ".txt", std::ios_base::app);
         if (file.fail()) std::cerr << "Can't open output file\n";
         else
         {
@@ -169,11 +85,8 @@ int main(int argc, char *argv[])
             file << dt << "\n";
             file.close();
         }
-        if (verbose)
-        {
-            std::cout.precision(std::numeric_limits<double>::max_digits10);
-            std::cout << dt << "\n";
-        }
+        std::cout.precision(std::numeric_limits<double>::max_digits10);
+        std::cout << dt << "\n";
     }
 
     delete comp;
